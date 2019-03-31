ffab6c6a68dd33f6de618e1cbd5b73a79c3a69aa
David Russell <russeldj@clarkson.edu>
2019-03-31 11:46:56 -0400

    Added visualization from intermediate files

--- source/heatmap.py
+++ source/heatmap.py
@@ -23,6 +23,7 @@ JSON_FOLDER="data/Makerspace_mentors/jsons"
 REF_IMAGE_FNAME = "data/Makerspace_mentors/ref_img.jpeg"
 REF_VIDEO_FNAME = "data/Makerspace_mentors/ref_video.avi"
 OVERHEAD_IMAGE_FNAME = "data/Makerspace_mentors/Makerspace.PNG"
+EXAMPLE_NPY_HEATMAP = "results/unnormalized_heatmap0_100.npy"
 
 def parse_args():
     parser = argparse.ArgumentParser()
@@ -30,6 +31,7 @@ def parse_args():
     parser.add_argument("--refrence-image", help="An image of the space", default=REF_IMAGE_FNAME)
     parser.add_argument("--overhead-image", help="The floorplan of your space", default=OVERHEAD_IMAGE_FNAME)
     parser.add_argument("--choose-homography-points", action="store_true", default=False, help="Select new points to compute the homography")
+    parser.add_argument("--visualize-from-file", action="store_true", default=False, help="Visualize from a heatmap stored in a file")
     args = parser.parse_args()
     return args
 
@@ -38,7 +40,28 @@ def visualize(foldername=""):
     visualizer = KeypointVisualization.KeypointVisualization()
     visualizer.WritePointsToVideo(capture, "first_makerspace_vid.avi", REF_VIDEO_FNAME)
 
+def visualize_from_npy(filename, ref_img_fname=OVERHEAD_IMAGE_FNAME, colormap="inferno", heatmap_intensity=3.0, use_floormap=True):
+    data = np.load(filename)
+    data = data[...,2] # only one channel is visualized
+    data /= np.amax(data) # normalize
+    cmap = matplotlib.cm.ScalarMappable(cmap=colormap) # an object which turns scalars to colors
+    #cmap.set_array(data)
+    #cmap.autoscale()# norm the data from [0,1]
+    cmapped = cmap.to_rgba(data)
+    ref_im = cv2.imread(ref_img_fname) # read in the visualization image
 
+    #cv2.imshow("", cv2.cvtColor(cmapped[...,:3].astype(np.uint8), cv2.COLOR_BGR2RGB))
+    vis_heatmap = (cmapped[...,:3] * 255).astype(np.uint8)
+    vis_heatmap = cv2.cvtColor(vis_heatmap, cv2.COLOR_BGR2RGB)
+    if use_floormap: # visualize on the overhead view of the space
+        vis_im = ref_im.astype(np.uint16) + (vis_heatmap*heatmap_intensity).astype(np.uint16)
+    else:
+        vis_im = vis_heatmap 
+    vis_im = (vis_im / (np.amax(vis_im) / 255.0)).astype(np.uint8) # normalize to [0, 255]
+    cv2.imshow("", vis_im)
+    cv2.imwrite("colormapped.png", vis_im) 
+    cv2.waitKey(2000)
+    
 def compute_homography(image_fname=REF_IMAGE_FNAME, overhead_fname=OVERHEAD_IMAGE_FNAME, use_example_homography=True):
     """
     This interfaces with the user to get the relation between the room and the image
@@ -152,8 +175,14 @@ def test_warp(homography, input_im, output_shape):
     cv2.waitKey(1)
     cv2.destroyAllWindows()
 
-def plot_points(frames, homography, ref_image_fname=REF_IMAGE_FNAME, overhead_image_fname=OVERHEAD_IMAGE_FNAME, use_video=True, ref_video_fname=REF_VIDEO_FNAME, colormap="Blues"):
-    #cmap = matplotlib.cm.ScalarMappable(cmap=colormap) # an object which turns scalars to colors
+def select_floor_keypoint(person):
+    """
+    take a person object and returns the location in image space of the selected keypoint
+    """
+    #min(person, key=person.)
+    return max(person.items(), key=lambda x: x[1][1])[1] # note that this is image space coordinates
+    
+def plot_points(frames, homography, ref_image_fname=REF_IMAGE_FNAME, start_ind=0, stop_ind=100, overhead_image_fname=OVERHEAD_IMAGE_FNAME, use_video=True, ref_video_fname=REF_VIDEO_FNAME, colormap="Blues"):
     # load all of the data and open the subplot
     f, (ax1, ax2) = plt.subplots(1, 2)
     if use_video:
@@ -170,51 +199,44 @@ def plot_points(frames, homography, ref_image_fname=REF_IMAGE_FNAME, overhead_im
     overhead_heatmap = np.zeros_like(overhead_im, dtype=np.float64) # create an accumulator for the heatmap
     ref_heatmap_y, ref_heatmap_x = ref_heatmap.shape
     overhead_heatmap_y, overhead_heatmap_x, _ = overhead_heatmap.shape
-
-    for frame in frames:
-        _, ref_im = video.read()
-        ref_im = cv2.cvtColor(ref_im, cv2.COLOR_BGR2RGB)
+    print("the number of frames is {}".format(len(frames)))
+    
+    for frame in frames[start_ind:stop_ind]:
+        #_, ref_im = video.read()
+        #ref_im = cv2.cvtColor(ref_im, cv2.COLOR_BGR2RGB)
         for person in frame:
             #HACK selecting the right heel as the point we care about, this should be made more inteligent
             H = person["RHeel"]
+            H = select_floor_keypoint(person)
             if H.conf == 0: # this means it wasn't actually detected so we shouldn't plot it
                 continue # go to the next itteration of the loop
-            SCALE=1.0
-            ax1.scatter([H.x * SCALE], [H.y * SCALE]) # plot on the image just for visualization
+            ax1.scatter([H.x], [H.y]) # plot on the image just for visualization
 
-            ref_heatmap += 20 * gaussian(H.x * SCALE, H.y * SCALE, ref_heatmap_x, ref_heatmap_y) # increment the heatmap
+            ref_heatmap += gaussian(H.x, H.y, ref_heatmap_x, ref_heatmap_y) # increment the heatmap
 
             # convert to homogenous coordinates
-            homogenous = np.asarray([[H.x], [H.y], [1.0]])# check that it's really x, y
+            homogenous = np.asarray([[H.x], [H.y], [1.0]]) # add the last coordinate
             transformed = np.dot(homography, homogenous)
             transformed /= transformed[2] # normalize
 
-            gaussian_addition = 100 * gaussian(transformed[0], transformed[1], overhead_heatmap_x, overhead_heatmap_y)
+            gaussian_addition = gaussian(transformed[0], transformed[1], overhead_heatmap_x, overhead_heatmap_y)
 
-            print(sum(sum(gaussian_addition)))
-
-            overhead_heatmap[:,:,0] = overhead_heatmap[:,:,0] + gaussian_addition # increment the heatmap
-            overhead_heatmap[:,:,1] = overhead_heatmap[:,:,1] + gaussian_addition # increment the heatmap
+            #overhead_heatmap[:,:,0] = overhead_heatmap[:,:,0] + gaussian_addition # increment the heatmap
+            #overhead_heatmap[:,:,1] = overhead_heatmap[:,:,1] + gaussian_addition # increment the heatmap
             overhead_heatmap[:,:,2] = overhead_heatmap[:,:,2] + gaussian_addition # increment the heatmap
-            overhead_heatmap /= np.amax(overhead_heatmap) * 128 # normalize it to [1, 128]
 
-            print("The max of the heatmap is {}".format(np.amax(overhead_heatmap)))
-            vis = overhead_im / 2.0 + overhead_heatmap.astype(np.uint8)
-            cv2.imwrite("heatmap.png", overhead_heatmap)
-            #cv2.imshow("", overhead_heatmap[:,:,3])
-            cv2.waitKey(1)
-            cv2.imwrite("overhead.png", np.array(overhead_im))
-            ax2.imshow(vis.astype(np.uint8)) # add the heatmap, must be unit8 or it will think it's [0,1]
-            cv2.imshow("", vis.astype(np.uint8))
-            #print("showed {}, one pixel is {}".format(colormap, plasma_heatmap[100, 100, :]))
-            #plot the transformed ones
-            #ax2.scatter([transformed[0]], [transformed[1]])
-            plt.pause(0.005)
+    np.save("results/unnormalized_heatmap{}_{}.npy".format(start_ind, stop_ind), overhead_heatmap)
+    overhead_heatmap /= np.amax(overhead_heatmap) / 128.0 # normalize it to [1, 128] note that this counts recent points more heavily
+    print("The max of the heatmap is {}".format(np.amax(overhead_heatmap)))
+    vis = overhead_im / 2.0 + overhead_heatmap.astype(np.uint8)
+    cv2.imwrite("visualization{}_{}.png".format(start_ind, stop_ind), vis)
+    ax2.imshow(vis.astype(np.uint8)) # add the heatmap, must be unit8 or it will think it's [0,1]
+    cv2.imshow("", vis.astype(np.uint8))
+    plt.pause(0.005)
     ax1.set_title("foot locations overlayed on first image")
     ax2.set_title("foot locations in the room space")
-    plt.show()
-    plt.waitforbuttonpress()
-
+    #plt.show()
+    plt.close()
     
 def main():
     # there are going to be a few steps
@@ -224,12 +246,18 @@ def main():
     ## visualize the results 
     args = parse_args()
     #visualize(args.json_folder)
-    homography = compute_homography(args.refrence_image, args.overhead_image, not args.choose_homography_points)
-    print(homography)
-    #homography = np.zeros((3,3))
-    frames = load_jsons(args.json_folder)
-    plot_points(frames, homography, args.refrence_image)
+    if args.visualize_from_file:
+        visualize_from_npy(EXAMPLE_NPY_HEATMAP)
+    else:
+        homography = compute_homography(args.refrence_image, args.overhead_image, not args.choose_homography_points)
+        print(homography)
+        #homography = np.zeros((3,3))
+        frames = load_jsons(args.json_folder)
+        STEP_SIZE = 100
+        for i in range(100):
+            plot_points(frames, homography, args.refrence_image, i * STEP_SIZE, (i+1) * STEP_SIZE)
 
+            
 if __name__ == "__main__":
     parse_args()
     main()
